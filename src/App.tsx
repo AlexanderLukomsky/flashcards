c4bfd7b5bb4b5190a2119e73d8bb1f3e9da6e228
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-07-17 16:09:09 +0300

    fix: path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RegistrationForm } from './components/auth/forms/registration';
-import { SignInForm } from './components/auth/forms/signIn';
+import { SignInForm } from './components/auth/forms/sign-in';
 
 export const App = () => (
   <div

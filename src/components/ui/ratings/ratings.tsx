f85e21e714148d85da85adc89accc3c1b0b1c06b
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-07-19 16:59:19 +0300

    feat: rating

---
+++ src/components/ui/ratings/ratings.tsx
@@ -0,0 +1,34 @@
+import { StarIcon } from '@assets/icons';
+import { MouseEventHandler } from 'react';
+import clsx from 'clsx';
+import { Button } from '../button';
+import style from './style.module.scss';
+
+export type RatingsProps = { value?: number; onChange?: (index: number) => void };
+
+export const Ratings = ({ onChange, value = 0 }: RatingsProps) => {
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    onChange?.(+event.currentTarget.value);
+  };
+
+  return (
+    <div className={style.rating}>
+      {Array.from({ length: 5 }, (_, index) => {
+        const currentValue = index + 1;
+
+        return (
+          <Button
+            className={clsx(style.button, {
+              [style.selected]: currentValue <= value,
+            })}
+            key={currentValue}
+            value={currentValue}
+            variant="icon"
+            IconStart={<StarIcon />}
+            onClick={handleClick}
+          />
+        );
+      })}
+    </div>
+  );
+};
